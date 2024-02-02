9c4eed48bc780c548d43596b99ab15ff7bebcd67
Quelestnotrenom <ericjoye02@gmail.com>
2024-02-02 11:03:33 +0100

    exercice5

--- exercices/exercice5/client/javascripts/controllers/indexCtrl.js
+++ exercices/exercice5/client/javascripts/controllers/indexCtrl.js
@@ -32,6 +32,13 @@ function chargerTeamSuccess(data, text, jqXHR) {
 function chargerPlayerSuccess(data, text, jqXHR) {
 	// Appelé lorsque la liste des joueurs est reçue
     var cmbJoueurs = document.getElementById("cmbJoueurs");
+    
+    $(data).find("joueur").each(function() {
+        var joueur = new Joueur();
+        joueur.setNom($(this).find("nom").text());
+        joueur.setPoints($(this).find("points").text());
+        cmbJoueurs.options[cmbJoueurs.options.length] = new Option(joueur, JSON.stringify(joueur));
+    });
 	// A COMPLETER!!! selon la logique suivante:
 	// cmbJoueurs.options[cmbJoueurs.options.length] = new Option(<ce qui sera affiché>, <la valeur de la cellule>));
 }
@@ -80,12 +87,14 @@ $(document).ready(function() {
     cmbEquipes.change(function(event) {
         equipe = this.options[this.selectedIndex].value;
         chargerPlayers(JSON.parse(equipe).pk, chargerPlayerSuccess, chargerPlayerError);
+       
     });
 	
 	// Ce qui se passe lorsque l'on sélectionne une joueur
     cmbJoueurs.change(function(event) {
         joueur = this.options[this.selectedIndex].value;
         alert(JSON.parse(joueur).nom + ": " + JSON.parse(joueur).points + " points");
+
     });
 });
 
